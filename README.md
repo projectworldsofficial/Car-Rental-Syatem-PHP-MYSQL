7389a8b372332f6b076f1e30dab59732092b3368
projectworlds <projectworlds@users.noreply.github.com>
2018-08-25 02:19:02 +0530

    initiate

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Car Rental Syatem PHP MYSQL
+# CarRental
+Car rental management website based on php as a backend for univ web project
---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+	<li class="about">
+					<p>projectworlds team is an organized company that rents cars and other vehicles to clients at lower costs. We we are here to serve every indian Citizen</p>
+					<ul>
+						<li><a href="http://facebook.com/projectworldsin" class="facebook" target="_blank"></a></li>
+						<li><a href="http://twitter.com/projectworlds32" class="twitter" target="_blank"></a></li>
+						<li><a href="http://plus.google.com/projectworlds32" class="google" target="_blank"></a></li>
+						<li><a href="#" class="skype"></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		<div class="copyrights wrapper">
+			Copyright &copy; <?php echo date("Y")?> All Rights Reserved | Designed by <a href= "http://projectworlds.in/">projectworlds.</a>
+		</div>
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
